26514f079d5b79006fc847fecdd35f933da1c818
Robert DeLuca <robertd@izea.com>
2014-11-25 17:29:38 -0500

    Add to dummy app

--- addon/views/dk-select.js
+++ addon/views/dk-select.js
@@ -1,3 +1,4 @@
+/* global Dropkick */
 import Ember from 'ember';
 
 export default Ember.Select.extend({
---
+++ app/controllers/index.js
@@ -0,0 +1,5 @@
+import Ember from "ember";
+
+export default Ember.ObjectController.extend({
+  options: ["ewuir hewr", "ewfioweoi wef", "eorifioerio wef"]
+});
