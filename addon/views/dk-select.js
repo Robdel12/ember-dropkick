ef9a95490e8329a01e3780166447eb31f5c618e1
Robert DeLuca <robertd@izea.com>
2014-11-26 11:07:50 -0500

    Put testing files in the right place and fixed view js

--- addon/views/dk-select.js
+++ addon/views/dk-select.js
@@ -1,20 +1,20 @@
 /* global Dropkick */
 import Ember from 'ember';
 
-export default Ember.View.extend(Ember.Select, {
+export default Ember.Select.extend({
 
   _dk: null,
   settings: {},
-  
+
   init: function() {
     var self = this,
       oldChange = this.get('settings.change');
-    
+
     this._super();
-    
+
     this.set('settings.change', function() {
       self.trigger('change');
-      
+
       if (typeof oldChange === 'function') {
         oldChange.call(this);
       }
@@ -28,11 +28,11 @@ export default Ember.View.extend(Ember.Select, {
       this._dk.select(this._dk.search(this.get('value'))[0]);
     }
   }.observes('value'),
-  
+
   createDk: function() {
     this._dk = new Dropkick(this.get('element'), this.get('settings'));
   }.on('didInsertElement'),
-  
+
   destroyDk: function() {
     if (this._dk !== null) {
       this._dk.dispose();
