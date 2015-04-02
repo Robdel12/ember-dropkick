b7fe2c761e3b0f7992cd4e91c77bcce7657a5861
Robert DeLuca <robertd@izea.com>
2015-04-02 14:16:24 -0400

    Setting up ember try

---
+++ config/ember-try.js
@@ -0,0 +1,22 @@
+module.exports = {
+  scenarios: [
+    {
+      name: 'Ember 1.10',
+      dependencies: {
+        'ember': '1.10.0'
+      }
+    },
+    {
+      name: 'Ember 1.11.0',
+      dependencies: {
+        'ember': '1.11.0'
+      }
+    },
+    {
+      name: 'Ember canary',
+      dependencies: {
+        'ember': 'canary'
+      }
+    }
+  ]
+};
