3ed48dad625f6677a81c181e1fb420a0d40ffe5f
Robert DeLuca <robertd@izea.com>
2015-04-04 20:18:30 -0500

    Add ember 1.11.1 to testing

--- config/ember-try.js
+++ config/ember-try.js
@@ -13,6 +13,12 @@ module.exports = {
       }
     },
     {
+      name: 'Ember 1.11.1',
+      dependencies: {
+        'ember': '1.11.1'
+      }
+    },
+    {
       name: 'Ember canary',
       dependencies: {
         'ember': 'canary'
